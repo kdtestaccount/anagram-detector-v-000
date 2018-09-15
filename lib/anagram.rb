66de44328a717a5ada2fd4f11d37f8c010f49d5b
kvdoherty <kvdoherty@gmail.com>
2018-09-15 20:00:10 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ def initialize(word)
  @word = word 
 end
 
-def self.match(possible_anagrams)
+def match(possible_anagrams)
   word_size = @word.size
   word_by_letter = @word.each_char.sort
   possible_anagrams.select{|w| anagram?(word_size, word_by_letter, w)}
