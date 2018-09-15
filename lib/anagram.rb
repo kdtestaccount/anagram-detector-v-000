c2e5f54d2d7f4228b0bc03d5cc682ac4c14689d5
kvdoherty <kvdoherty@gmail.com>
2018-09-15 19:53:00 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+class Anagram
+  
+attr_accessor :total, :discount, :item
+  
+def initialize()
+  
+
+
+
+  @total = 0
+  @discount = discount
+  @item = []
+  @last_item = 0
+end
+
+  
+  
+  
+  
+  
+  
+  
+  
+end
