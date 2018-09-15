0370c91d307a82fc613363f4e9c0827873a163f1
kvdoherty <kvdoherty@gmail.com>
2018-09-15 19:59:30 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,10 +8,12 @@ def initialize(word)
 end
 
 def self.match(possible_anagrams)
+  word_size = @word.size
   word_by_letter = @word.each_char.sort
+  possible_anagrams.select{|w| anagram?()}
 end
   
-  
+
   
   
   
