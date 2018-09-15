e7a461fe68b8cd955641f29123ebe0f87f3459b2
kvdoherty <kvdoherty@gmail.com>
2018-09-15 19:57:33 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,15 +4,7 @@ class Anagram
 attr_accessor :word
   
 def initialize(word)
-  
-end
-
-  
-  def initialize(discount=0)
-  @total = 0
-  @discount = discount
-  @item = []
-  @last_item = 0
+ @word = word 
 end
 
 def self.match(possible_anagrams)
